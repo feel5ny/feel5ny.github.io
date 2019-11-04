329bb241a48dbba8a1944fad22c3b5f101ce9c11
joy.kim <joy.kim@goodoc.co.kr>
2019-11-04 23:57:14 +0900

    Site updated: 2019-11-04 23:57:09

--- js/main.js
+++ js/main.js
@@ -85,7 +85,7 @@
             // If more link not exists
             if ($('.main-nav-more').length == 0) {
                 $(['<li class="main-nav-list-item top-level-menu main-nav-more">',
-                    '<a class="main-nav-list-link" href="javascript:;">More</a>',
+                    '<a class="main-nav-list-link" href="javaScript:;">More</a>',
                     '<ul class="main-nav-list-child">',
                     '</ul></li>'].join('')).appendTo($('#main-nav'));
                 // Bind hover event
