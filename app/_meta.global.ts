8729716c3287fd662a79972a8a5d718a0989a9f7
phucbm <hiphucbui@gmail.com>
2025-02-01 12:00:49 +0700

    refactor: use app directory structure, add comments

---
+++ app/_meta.global.ts
@@ -0,0 +1,15 @@
+export default {
+    '*': {
+        type: 'page'
+    },
+    index: 'Home',
+    about: 'About',
+    posts: {
+        type: 'page',
+        items: {
+            draft: {
+                display: 'hidden'
+            }
+        }
+    }
+}
--- content/_meta.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    '*': {
-        type: 'page'
-    },
-    index: 'Home',
-    about: 'About'
-}
