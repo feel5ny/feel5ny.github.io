f2cdb04cf29ba8d4c3e7be7b3081540f5c17530f
Joy Kim <feel5.nayoung@gmail.com>
2025-12-31 21:22:35 +0900

    오류 수정

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -8,7 +8,6 @@ export async function GET() {
   const now = new Date().toISOString();
 
   const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
-<?xml-stylesheet type="text/xsl" href="sitemap.xsl"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <sitemap>
         <loc>${baseUrl}/post-sitemap.xml</loc>
